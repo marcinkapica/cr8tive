f1b25ffe423133a8adad93fd1543eaff2461b834
martions <martions@gmail.com>
2017-07-11 00:02:05 +0200

    footer form and icon styling

--- app/scripts/google-map.js
+++ app/scripts/google-map.js
@@ -1,11 +1,11 @@
   function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var shire = {lat: -37.8576183, lng: 175.6798615};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
+      zoom: 10,
+      center: shire
     });
     var marker = new google.maps.Marker({
-      position: uluru,
+      position: shire,
       map: map
     });
   }
